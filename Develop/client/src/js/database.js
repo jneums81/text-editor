9a2f18f289e47e535e88a2c6edacfddf61d55456
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-11-20 18:55:58 -0600

    cloned data and added logic to database.js

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,46 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.log('Put to the database');
+  try {
+    const textDb = await openDB('jate', 1);
+    const tx = textDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.add({ content });
+    const result = await request;
+    console.log('Data saved to the database', result);
+  } catch (error) {
+    console.error('Error putting data to the database', error);
+  }
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  try {
+    const textDb = await openDB('jate', 1);
+    const tx = textDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('Retrieved data from the database', result);
+  } catch (error) {
+    console.error('Error getting data from the database', error);
+  }
+};
+
+initdb();
+
